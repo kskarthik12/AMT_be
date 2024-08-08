498269b4dfdebd548ec785f05da30cae2ba6760d
Karthik K <125796499+kskarthik12@users.noreply.github.com>
2024-08-08 18:52:29 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,18 +1,12 @@
 import express from 'express'
 import dotenv from 'dotenv'
-const path = require('path');
 import cors from 'cors'
 import AppRoutes from './src/routes/index.js'
 dotenv.config()
 
 const app = express();
 
-app.use(express.static(path.join(__dirname, 'public')));
 
-// Default route to serve index.html
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
-});
 
 app.use(cors());
 app.use(express.json());
